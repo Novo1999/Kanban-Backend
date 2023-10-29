e08dca7981b93ecce65d634c8deb92989aee942d
Novo1999 <neptune.navo@gmail.com>
2023-10-29 20:31:23 +0600

    update

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -37,7 +37,7 @@ export const login = async (req: Request, res: Response) => {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none',
+    sameSite: 'strict',
   })
 
   return res.status(StatusCodes.OK).json({ msg: 'user logged in' })
